a2611e08bb0cb1ea4722ace21a9e4b7d80704d16
Neal Powell <pashneal@gmail.com>
2024-01-10 19:20:44 -0500

    EV stuff seems to be working

--- src/eval/rank.rs
+++ src/eval/rank.rs
@@ -17,7 +17,7 @@ impl HandRanker {
     pub fn rank7(&self, cards: &[u8]) -> u32 {
         unsafe {
             let func: Symbol<unsafe extern "C" fn(u8, u8, u8, u8, u8, u8, u8) -> u32> =
-                self.library.get(b"get_rank").unwrap();
+                self.library.get(b"get_rank7").unwrap();
             func(
                 cards[0], cards[1], cards[2], cards[3], cards[4], cards[5], cards[6],
             )
@@ -26,20 +26,41 @@ impl HandRanker {
 
     pub fn rank8(&self, cards: &[u8]) -> u32 {
         unsafe {
-            let func: Symbol<unsafe extern "C" fn(u8, u8, u8, u8, u8, u8, u8) -> u32> =
-                self.library.get(b"get_rank").unwrap();
-            let mut max_rank = 0;
-            for i in 0..8 {
-                let mut cards8 = cards.iter().map(|c| *c).collect::<Vec<u8>>();
-                cards8.swap(i, 7);
-                let rank = func(
-                    cards8[0], cards8[1], cards8[2], cards8[3], cards8[4], cards8[5], cards8[6],
-                );
-                if rank > max_rank {
-                    max_rank = rank;
-                }
-            }
-            max_rank
+            let func: Symbol<unsafe extern "C" fn(u8, u8, u8, u8, u8, u8, u8, u8) -> u32> =
+                self.library.get(b"get_rank8").unwrap();
+            func(
+                cards[0], cards[1], cards[2], cards[3], cards[4], cards[5], cards[6], cards[7]
+            )
+        }
+    }
+
+    pub fn rollout_2_7(&self, cards : &[u8], iterations : u32) -> f64 {
+        unsafe {
+            let func: Symbol<unsafe extern "C" fn(u8, u8, u32) -> f64> =
+                self.library.get(b"rollout_2_7").unwrap();
+            func(cards[0], cards[1], iterations)
+        }
+    }
+    pub fn rollout_2_8(&self, cards : &[u8], iterations : u32) -> f64 {
+        unsafe {
+            let func: Symbol<unsafe extern "C" fn(u8, u8, u32) -> f64> =
+                self.library.get(b"rollout_2_8").unwrap();
+            func(cards[0], cards[1], iterations)
+        }
+    }
+
+    pub fn rollout_bid_win(&self, hand: &[u8] ,  community_cards : &[u8], iterations: u32) -> f64 {
+        unsafe {
+            let func: Symbol<unsafe extern "C" fn(u8, u8, u8, u8, u8, u32) -> f64> =
+                self.library.get(b"rollout_bid_win").unwrap();
+            func(hand[0], hand[1], community_cards[0], community_cards[1], community_cards[2], iterations)
+        }
+    }
+    pub fn rollout_bid_loss(&self, hand: &[u8] ,  community_cards : &[u8], iterations: u32) -> f64 {
+        unsafe {
+            let func: Symbol<unsafe extern "C" fn(u8, u8, u8, u8, u8, u32) -> f64> =
+                self.library.get(b"rollout_bid_loss").unwrap();
+            func(hand[0], hand[1], community_cards[0], community_cards[1], community_cards[2], iterations)
         }
     }
 }
@@ -47,6 +68,85 @@ impl HandRanker {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test] 
+    fn test_rollout_bid() {
+        let hand_ranker = HandRanker::new();
+        let hand = [
+            Card::new("Kc").to_usize().unwrap() as u8, 
+            Card::new("Kd").to_usize().unwrap() as u8
+        ];
+        let community_cards = [
+            Card::new("Kh").to_usize().unwrap() as u8, 
+            Card::new("Ks").to_usize().unwrap() as u8, 
+            Card::new("4h").to_usize().unwrap() as u8
+        ];
+        let iterations = 10_000;
+        let strong = hand_ranker.rollout_bid_win(&hand, &community_cards, iterations);
+        let weak = hand_ranker.rollout_bid_loss(&hand, &community_cards, iterations);
+        println!("Strong (bid): {}", strong);
+        println!("Weak (bid): {}", weak);
+        assert!(strong > weak);
+
+    }
+    #[test]
+    fn rollout_with_8_better_than_7_straight() {
+        let hand_ranker = HandRanker::new();
+        let card1 = Card::new("8c").to_usize().unwrap() as u8;
+        let card2 = Card::new("9c").to_usize().unwrap() as u8;
+        let cards = [card1, card2];
+        let iterations = 10_000;
+        let weak = hand_ranker.rollout_2_7(&cards, iterations);
+        let strong = hand_ranker.rollout_2_8(&cards, iterations);
+        println!("Weak (straight): {}", weak);
+        println!("Strong (straight): {}", strong);
+        assert!(weak < strong);
+    }
+    #[test]
+    fn rollout_with_8_better_than_7_kings() {
+        let hand_ranker = HandRanker::new();
+        let card1 = Card::new("Kc").to_usize().unwrap() as u8;
+        let card2 = Card::new("Kd").to_usize().unwrap() as u8;
+        let cards = [card1, card2];
+        let iterations = 100_000;
+        let time = std::time::Instant::now();
+        let weak = hand_ranker.rollout_2_7(&cards, iterations);
+        let strong = hand_ranker.rollout_2_8(&cards, iterations);
+        println!("Weak (kings): {}", weak);
+        println!("Strong (kings): {}", strong);
+        assert!(weak < strong);
+    }
+
+
+    #[test]
+    fn rollout_with_8_better_than_7_aces() {
+        let hand_ranker = HandRanker::new();
+        let card1 = Card::new("Ad").to_usize().unwrap() as u8;
+        let card2 = Card::new("Ah").to_usize().unwrap() as u8;
+        let cards = [card1, card2];
+        let iterations = 100_000;
+        let weak = hand_ranker.rollout_2_7(&cards, iterations);
+        let strong = hand_ranker.rollout_2_8(&cards, iterations);
+        println!("Weak: {}", weak);
+        println!("Strong: {}", strong);
+        assert!(weak < strong);
+    }
+
+    #[test]
+    fn can_call_rollout_function() {
+        let hand_ranker = HandRanker::new();
+        let card1 = Card::new("Kc").to_usize().unwrap() as u8;
+        let card2 = Card::new("Jd").to_usize().unwrap() as u8;
+        let cards = [card1, card2];
+        let iterations = 10_000;
+        // Time it!
+        let start = std::time::Instant::now();
+        let result = hand_ranker.rollout_2_7(&cards, iterations);
+        let end = std::time::Instant::now();
+        println!("Result: {}", result);
+        println!("Time: {:?}", end - start);
+        assert!(result > 0.0); // Not necessarily true, but highly highly unlikely
+        assert!(result <= 1.0);
+    }
 
     #[test]
     fn test_load_libso() {
@@ -54,6 +154,45 @@ mod tests {
         let cards = [1, 2, 3, 4, 5, 6, 7];
         let rank = hand_ranker.rank7(&cards);
         assert_eq!(rank, 7440);
+    }
+
+    #[test]
+    fn test_royal_flush_8() {
+        let hand_ranker = HandRanker::new();
+        // test with royal flush so we are sure it is working (only one possible
+        // best hand in set of 8 if royal flush)
+        let cards  = [
+            Card::new("2d"),
+            Card::new("4d"),
+            Card::new("Kc"),
+            Card::new("Qc"),
+            Card::new("Jc"),
+            Card::new("3d"),
+            Card::new("Tc"),
+            Card::new("Ac"),
+        ];
+
+        let cards = cards
+            .iter()
+            .map(|c| c.to_usize().unwrap() as u8)
+            .collect::<Vec<u8>>();
+        let rank = hand_ranker.rank8(&cards);
+        let cards7  = [
+            Card::new("4h"),
+            Card::new("Ac"),
+            Card::new("9d"),
+            Card::new("Kc"),
+            Card::new("Qc"),
+            Card::new("Jc"),
+            Card::new("Tc"),
+        ];
+
+        let cards7 = cards7
+            .iter()
+            .map(|c| c.to_usize().unwrap() as u8)
+            .collect::<Vec<u8>>();
+        let rank7 = hand_ranker.rank7(&cards7);
+        assert_eq!(rank, rank7);
     }
 
     #[test]
@@ -68,13 +207,13 @@ mod tests {
     fn test_card_interpretation() {
         let hand_ranker = HandRanker::new();
         let royal_flush = [
-            Card::new("Ac"),
             Card::new("Kc"),
             Card::new("Qc"),
             Card::new("Jc"),
             Card::new("Tc"),
             Card::new("2d"),
             Card::new("3d"),
+            Card::new("Ac"),
         ];
         let royal_flush = royal_flush
             .iter()
